40992824d3c1bef8bcc564eada67a9a137eb7a9a
LauraAlonsoLedo <91979536+LauraAlonsoLedo@users.noreply.github.com>
2021-11-20 18:36:51 +0100

    Update logica.php
    Ejercicios 1,2,3,4 terminados

--- PHP/logica.php
+++ PHP/logica.php
@@ -1,11 +1,38 @@
 <?php
+define("RENUM",'/[0-9]+/');
+define("RELET",'/[a-zA-Z]+/');
+
+function checkTelefono($telf){
+    if(preg_match(RELET,$telf)!=0){
+        echo "El teléfono no puede contener letras.<br>";
+    }else{
+        if(strlen($telf)!=9){
+            echo "Tienes que introducir 9 números en el teléfono. <br>";
+        }
+    }
+}
+function checkCP($cp){
+    if(preg_match(RELET,$cp)!=0){
+        echo "El código postal puede contener letras <br>";
+    }else{
+        if(strlen($cp)!=9){
+            echo "Tienes que introducir 5 números en el código postal. <br>";
+        }
+    }
+}
+
 function solo_numeros($enter){
     $regex ="/([0-9]){5}/";
     return preg_match($regex,$enter);
 }
 
-function solo_letras($enter){
-    $regex ='/[a-zA-Z]/';
+function busca_numeros($enter){
+    $regex ='/[0-9]+/';
+    return preg_match($regex,$enter);
+}
+
+function busca_letras($enter){
+    $regex ='/[a-zA-Z]+/';
     return preg_match($regex,$enter);
 }
 
@@ -22,22 +49,34 @@ function web($enter){
     $regex = '#((https?|ftp)://(\S*?\.\S*?))([\s)\[\]{},;"\':<]|\.\s|$)#i';
     return preg_match($regex,$enter);
 }
-
+echo '<pre>';
+//Mostrar todo el array
+print_r($_POST);
+echo '</pre>';
 if (isset($_POST['enviar'])) {
-
-        if (solo_numeros($_POST['cp']) == 0) {
-            echo  "codigo postal no valido <br>";
-        }
-
-        if (email($_POST['email']) == 0) {
-            echo 'mail no valido <br>';
-        }
-
-        if (contrasena($_POST['psw']) == 0) {
-            echo 'contraseña no valida <br>';
-        }
-
-       if(web($_POST['web']) == 0){
-           echo 'web no valida <br>';
-       }
+    checkTelefono($_POST['telefono']);
+    checkCP($_POST['cp']);
+    if(busca_numeros($_POST['nombre']) != 0){
+        echo "El nombre no puede contener números. <br>";
+    }
+    if(busca_numeros($_POST['apellidos']) != 0){
+        echo "Los apellidos no pueden contener números. <br>";
+    }
+    if(busca_numeros($_POST['ciudad']) != 0){
+        echo "La ciudad no puede contener números. <br>";
+    }
+    if (solo_numeros($_POST['cp']) == 0) {
+        echo  "Código postal no válido. <br>";
+    }
+    if (email($_POST['email']) == 0) {
+        echo 'Email no válido <br>';
+    }
+    if (contrasena($_POST['psw']) == 0) {
+        echo 'Contraseña no válida. <br>';
+    }
+    if(web($_POST['web']) == 0){
+        echo 'Web no válida. <br>';
+    }
 }
+
+?>
