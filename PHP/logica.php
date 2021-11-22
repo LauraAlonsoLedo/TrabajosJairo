1ab2b1f29bcc171c5017b82c26622a69140a334c
DavidNavarroV <davidnavazq@gmail.com>
2021-11-22 01:22:28 +0100

    Final del proyecto

--- PHP/logica.php
+++ PHP/logica.php
@@ -1,82 +1,180 @@
+<!doctype html>
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Formulario</title>
+    <link rel="stylesheet" href="../CSS/CSSFormulario.css">
+</head>
+<body>
 <?php
+//creamos una variable para saber si el formulario tiene algún error
+$correcto = true;
+//Y replicamos la misma funcion
+function provincias()
+{
+    //Para ello nos creamos la variable a utilizar
+    $provincias = [];
+    //Y utilizamos la funcion file_get_contents para extraer los datos
+    $string = file_get_contents("Provincias.txt");
+
+    $array = explode("\n", $string);
+    foreach ($array as $fila) {
+        $item = explode(" ", $fila);
+        $provincias[] = [
+            'numero' => $item[1],
+            'nombre' => str_replace("_", " ", $item[0])
+        ];
+    }
+    //Devolvemos la variable que almacena los datos
+    return $provincias;
+}
+//Definimos los patrones necesarios mas adelante
 define("RENUM",'/[0-9]+/');
 define("RELET",'/[a-zA-Z]+/');
 
+
+// Y creamos una función que compruebe si el formato del número de teléfono es válido
 function checkTelefono($telf){
+    //llamamos a la variable global correcto
+    global $correcto;
+    //Y comprobamos si el valor introducido cumple los parámetros si no mandamos un mensaje de error y aplicamos correcto a falso
     if(preg_match(RELET,$telf)!=0){
-        echo "El teléfono no puede contener letras.<br>";
+        echo " <p class = 'err'> El teléfono no puede contener letras.</p>";
+        $correcto = false;
+
     }else{
+        //Ahora comprobamos que la longitud del valor sea exactamente igual a nueve de no ser así mandamos un mensaje de error y aplicamos correcto a falso
         if(strlen($telf)!=9){
-            echo "Tienes que introducir 9 números en el teléfono. <br>";
+            echo " <p class = 'err'> Tienes que introducir 9 números en el teléfono. </p>";
+            $correcto = false;
+
         }
     }
 }
+// Y creamos una función que compruebe si el formato del código postal es válido
 function checkCP($cp){
+//llamamos a la variable global correcto
+    global $correcto;
+    // creamos una variable que almacene el valor de $_POST['cp'] con un valor string
+    $cpf = strval($_POST['cp']);
+
+    //Y comprobamos si el valor introducido cumple los parámetros si no mandamos un mensaje de error y aplicamos correcto a falso
     if(preg_match(RELET,$cp)!=0){
-        echo "El código postal puede contener letras <br>";
-    }else{
-        if(strlen($cp)!=9){
-            echo "Tienes que introducir 5 números en el código postal. <br>";
+        echo " <p class = 'err'> El código postal no puede contener letras </p>";
+        $correcto = false;
+    }else {
+        //Ahora comprobamos que la longitud del valor sea exactamente igual a nueve de no ser así mandamos un mensaje de error y aplicamos correcto a falso
+        if (strlen($cp) != 5) {
+            echo " <p class ='err'> Tienes que introducir 5 números en el código postal. </p>";
+            $correcto = false;
+        } else {
+            //Ahora comprobaremos que el codigo de provincia corresponde con el principio del codigo postal
+            foreach (provincias() as $provincias) {
+                //primero debemos saber que provincia se ha marcado
+                if ($provincias['nombre'] == $_POST['Provincia']) {
+                    //Y después comprobar si se cumple o no las condiciones
+
+                    if ($provincias['numero'] != substr($cpf, 0, 2)) {
+                        echo " <p class ='err'> el codigo postal no corresponde con la Provincia seleccionada </p> ";
+                        $correcto = false;
+
+                    }
+                }
+            }
         }
     }
-}
 
-function solo_numeros($enter){
-    $regex ="/([0-9]){5}/";
-    return preg_match($regex,$enter);
 }
-
+// Esta función comprueba si el valor está compuesto solo por números
 function busca_numeros($enter){
     $regex ='/[0-9]+/';
     return preg_match($regex,$enter);
 }
-
-function busca_letras($enter){
-    $regex ='/[a-zA-Z]+/';
-    return preg_match($regex,$enter);
+// Esta función comprueba si el valor está compuesto solo por números y letras
+function letrasyNumeros($enter){
+    global $correcto;
+    $regex ='/[a-zA-Z0-9]+/';
+    if (preg_match($regex,$enter) != 1){
+        $correcto = false;
+        echo " <p class ='err'> La direccion solo puede contener letras y numeros </p>";
+    }
 }
-
+//Esta función comprueba si el valor introducido cumple con las reglas de formato de un email
 function email($enter){
-    return filter_var($enter, FILTER_VALIDATE_EMAIL);
+    global $correcto;
+    if( filter_var($enter, FILTER_VALIDATE_EMAIL) == 0){
+        $correcto = false;
+        echo '<p class ="err"> Email no válido </p>';
+    }
 }
-
+//Esta función comprueba si el valor introducido cumple con las siguientes condiciones:
+//Al menos un número
+//Al menos una mayúscula
+//Al menos una minúscula
+//Al menos un caracter especial
+//Mínimo 8 caracteres
+//Maximo 16 caracteres
 function contrasena($enter){
+    global $correcto;
     $regex = "/((?=.*?[A-Z])(?=.*?[a-z])(?=.*?[0-9])(?=.*?[^A-Za-z0-9])).{8,16}/";
-    return preg_match($regex,$enter);
+    if (preg_match($regex,$enter) != 1) {
+        $correcto = false;
+        echo "<p class ='err'> Contraseña no válida. </p>";
+    }
 }
-
+//Esta función comprueba si el valor introducido cumple con las reglas de formato de una página web
 function web($enter){
+    global $correcto;
     $regex = '#((https?|ftp)://(\S*?\.\S*?))([\s)\[\]{},;"\':<]|\.\s|$)#i';
-    return preg_match($regex,$enter);
+    if(preg_match($regex,$enter) == 0){
+        $correcto = false;
+        echo '<p class ="err"> Web no válida. </p>';
+
+    }
 }
-echo '<pre>';
-//Mostrar todo el array
-print_r($_POST);
-echo '</pre>';
+//Si pulsamos el botón enviar
 if (isset($_POST['enviar'])) {
+//Comprueba que el nombre no tenga números
+    if(busca_numeros($_POST['nombre']) != 0){
+        $correcto = false;
+        echo "<p class ='err'>El nombre no puede contener números. </p>";
+    }
+    //Comprueba que el apellido no tenga números
+    if(busca_numeros($_POST['apellidos']) != 0){
+        $correcto = false;
+        echo "<p class ='err'> Los apellidos no pueden contener números. </p>";
+    }
+    //Comprueba que la ciudad no tenga números
+    if(busca_numeros($_POST['ciudad']) != 0){
+        $correcto = false;
+        echo "<p class ='err'> La ciudad no puede contener números. </p>";
+    }
+// Y llamamos a las funciones con sus respectivos campos a comprobar
+    email($_POST['email']);
     checkTelefono($_POST['telefono']);
     checkCP($_POST['cp']);
-    if(busca_numeros($_POST['nombre']) != 0){
-        echo "El nombre no puede contener números. <br>";
-    }
-    if(busca_numeros($_POST['apellidos']) != 0){
-        echo "Los apellidos no pueden contener números. <br>";
-    }
-    if(busca_numeros($_POST['ciudad']) != 0){
-        echo "La ciudad no puede contener números. <br>";
-    }
-    if (solo_numeros($_POST['cp']) == 0) {
-        echo  "Código postal no válido. <br>";
-    }
-    if (email($_POST['email']) == 0) {
-        echo 'Email no válido <br>';
-    }
-    if (contrasena($_POST['psw']) == 0) {
-        echo 'Contraseña no válida. <br>';
-    }
-    if(web($_POST['web']) == 0){
-        echo 'Web no válida. <br>';
-    }
-}
+    letrasyNumeros($_POST['direccion']);
+    contrasena($_POST['password']);
+    web($_POST['web']);
+// Si no ha habido ningún fallo se envia correctamente el formulario y se redireccionará a la pagina del formulario
+    if($correcto == true ){
+        ?>
+        <div class ="final">  <h1 class ="b">Formulario enviado correctamente <br><br> <small>Se te redireccionará a la pagina del formulario en unos instantes</small></h1> <br>  <?php
+            header("refresh:3;url= ../index.php"); ?>
 
+        </div>
+        <?php
+        //Si ha habido fallos se mostraran y se tendrá la opción de volver ha introducir valores
+    }else{
+        ?>  <h1>Ha ocurrido un error</h1>
+        <a href="../index.php"> Pulsa aqui para rellenar el formulario de nuevo</a> <?php
+    }
+
+}
 ?>
+</body>
+</html>
+
--- index.php
+++ index.php
@@ -1,122 +1,96 @@
+<?php
+//Creamos la funcion que nos extrae las provincias y sus codigos del txt externo
+function provincias()
+{
+    //Para ello nos creamos la variable a utilizar
+    $provincias = [];
+    //Y utilizamos la funcion file_get_contents para extraer los datos
+    $string = file_get_contents("Provincias.txt");
+
+    $array = explode("\n", $string);
+    foreach ($array as $fila) {
+        $item = explode(" ", $fila);
+        $provincias[] = [
+            'numero' => $item[1],
+            'nombre' => str_replace("_", " ", $item[0])
+        ];
+    }
+    //Devolvemos la variable que almacena los datos
+    return $provincias;
+}
+//Creamos un array para utilizarlo de base para evitar estar tecleando en los campos
+$datos = ["David","Navarro","Calle Ejemplo","Mostoles","28932",123456789,"example@example.com","aB345678@","https://www.isepceu.es/"];
+
+?>
 <!doctype html>
-<html lang="en">
+<html lang="es">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport"
           content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <link rel="stylesheet" href="CSS/CSSFormulario.css">
-    <title>Document</title>
+    <title>Formulario</title>
 </head>
-<?php
-$_SESSION['provincias'] = [
-    ['nombre' => "Almería", 'código' => "04"],
-    ['nombre' => "Cádiz", 'código' => "11"],
-    ['nombre' => "Córdoba", 'código' => "14"],
-    ['nombre' => "Granada", 'código' => "18"],
-    ['nombre' => "Huelva", 'código' => "21"],
-    ['nombre' => "Jaén", 'código' => "23"],
-    ['nombre' => "Málaga", 'código' => "29"],
-    ['nombre' => "Huesca", 'código' => "22"],
-    ['nombre' => "Teruel", 'código' => "44"],
-    ['nombre' => "Zaragoza", 'código' => "50"],
-    ['nombre' => "Asturias", 'código' => "33"],
-    ['nombre' => "Islas Baleares", 'código' => "07"],
-    ['nombre' => "Las Palmas", 'código' => "35"],
-    ['nombre' => "Sta. Cruz de Tenerife", 'código' => "38"],
-    ['nombre' => "Cantabria", 'código' => "39"],
-    ['nombre' => "Ávila", 'código' => "05"],
-    ['nombre' => "Burgos", 'código' => "09"],
-    ['nombre' => "León", 'código' => "24"],
-    ['nombre' => "Palencia", 'código' => "34"],
-    ['nombre' => "Salamanca", 'código' => "37"],
-    ['nombre' => "Segovia", 'código' => "40"],
-    ['nombre' => "Soria", 'código' => "42"],
-    ['nombre' => "Valladolid", 'código' => "47"],
-    ['nombre' => "Zamora", 'código' => "49"],
-    ['nombre' => "Albacete", 'código' => "02"],
-    ['nombre' => "Ciudad Real", 'código' => "13"],
-    ['nombre' => "Cuenca", 'código' => "16"],
-    ['nombre' => "Guadalajara", 'código' => "19"],
-    ['nombre' => "Toledo", 'código' => "45"],
-    ['nombre' => "Barcelona", 'código' => "08"],
-    ['nombre' => "Gerona", 'código' => "17"],
-    ['nombre' => "Lérida", 'código' => "25"],
-    ['nombre' => "Tarragona", 'código' => "43"],
-    ['nombre' => "Alicante", 'código' => "03"],
-    ['nombre' => "Castellón", 'código' => "12"],
-    ['nombre' => "Valencia", 'código' => "46"],
-    ['nombre' => "Badajoz", 'código' => "06"],
-    ['nombre' => "Cáceres", 'código' => "10"],
-    ['nombre' => "La Coruña", 'código' => "15"],
-    ['nombre' => "Lugo", 'código' => "27"],
-    ['nombre' => "Ourense", 'código' => "32"],
-    ['nombre' => "Pontevedra", 'código' => "36"],
-    ['nombre' => "Madrid", 'código' => "28"],
-    ['nombre' => "Murcia", 'código' => "30"],
-    ['nombre' => "Navarra", 'código' => "31"],
-    ['nombre' => "Álava", 'código' => "01"],
-    ['nombre' => "Vizcaya", 'código' => "48"],
-    ['nombre' => "Guipúzcoa", 'código' => "20"],
-    ['nombre' => "La Rioja", 'código' => "26"],
-    ['nombre' => "Ceuta", 'código' => "51"],
-    ['nombre' => "Melilla", 'código' => "52"]
-];
-?>
+
 <body>
+<!-- Empezamos con la estructura del formulario -->
+
 <h1>Formulario de Entrada de datos</h1>
 <form action="PHP/logica.php" method="post">
-    <table>
+    <table cellspacing="5">
         <th rowspan="1">
             <td><h3>Datos del Formulario</h3></td>
         </th>
         <tr>
             <td><label>Nombre: </label></td>
-            <td><input type="text" name="nombre"></td>
+            <td><input type="text" name="nombre" value ='<?php echo $datos[0] ?>' required></td>
         </tr>
         <tr>
             <td><label>Apellidos: </label></td>
-            <td><input type="text" name="apellidos"></td>
+            <td><input type="text" name="apellidos" value ='<?php echo $datos[1] ?>' required></td>
         </tr>
         <tr>
             <td><label>Dirección: </label></td>
-            <td><input type="text" name="direccion"></td>
+            <td><input type="text" name="direccion" value ='<?php echo $datos[2] ?>' required></td>
         </tr>
         <tr>
             <td><label>Ciudad: </label></td>
-            <td><input type="text" name="direccion"></td>
+            <td><input type="text" name="ciudad" value ='<?php echo $datos[3] ?>' required></td>
         </tr>
         <tr>
             <td><label>Provincia: </label></td>
-            <td><select>
+            <td><select name="Provincia">
                 <?php
-                foreach ($_SESSION['provincias'] as $provincias){
-                    echo "<option value ='".$provincias['código']."'>".$provincias['nombre']."</option>";
-                }
-                ?></td>
+                //utilizamos un foreach para evitar crearnos muchas etiquetas option
+                foreach (provincias() as $provincias){
+                    ?> <option> <?php echo $provincias['nombre'];}?></option>  </select>
+                </td>
         </tr>
         <tr>
             <td><label>Código Postal: </label></td>
-            <td><input type="text" name ="cp"></td>
+            <td><input type="text" name ="cp" value ='<?php echo $datos[4] ?>' required></td>
         </tr>
         <tr>
             <td><label>Teléfono: </label></td>
-            <td><input type="text" name="telefono"></td>
+            <td><input type="text" name="telefono" value ='<?php echo $datos[5] ?>' required></td>
         </tr>
         <tr>
             <td><label>Email: </label></td>
-            <td><input type="text" name="email"></td>
+            <td><input type="text" name="email" value ='<?php echo $datos[6] ?>' required></td>
         </tr>
         <tr>
             <td><label>Password: </label></td>
-            <td><input type="password" name="password"></td>
+            <td><input type="password" name="password" value ='<?php echo $datos[7] ?>' required></td>
         </tr>
         <tr>
             <td><label>Web: </label></td>
-            <td><input type="text" name="web"></td>
+            <td><input type="text" name="web" value ='<?php echo $datos[8];?>' required></td>
         </tr>
         <tr>
-            <td><input type="submit" name="enviar" value="Enviar"><input type="reset" name="reiniciar" value="Reiniciar"></td>
+            <td><input type="submit" name="enviar" value="Enviar"> </td> <td><input type="reset" name="reiniciar" value="Reiniciar"></td>
+
+
         </tr>
     </table>
 </form>
