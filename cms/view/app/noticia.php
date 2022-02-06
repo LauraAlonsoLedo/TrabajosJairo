d1de702e7c5768ca3be0dec0a882ba48df32c072
LauraAlonsoLedo <91979536+LauraAlonsoLedo@users.noreply.github.com>
2022-02-06 13:12:34 +0100

    Add files via upload

---
+++ cms/view/app/noticia.php
@@ -0,0 +1,23 @@
+<h3>
+    <a href="<?php echo $_SESSION['home'] ?>" title="Inicio">Inicio</a> <span>| </span>
+    <a href="<?php echo $_SESSION['home'] ?>noticias" title="Noticias">Noticias</a> <span>| </span>
+    <span><?php echo $datos->nombre ?></span>
+</h3>
+<div class="row">
+    <article class="col s12">
+        <div class="card horizontal large noticia">
+            <div class="card-image">
+                <img src="<?php echo $_SESSION['public']."img/".$datos->imagen ?>" alt="<?php echo $datos->nombre ?>">
+            </div>
+            <div class="card-stacked">
+                <div class="card-content" style="font-size: 25px; text-align: center">
+                    <h2><strong><?php echo $datos->nombre ?></strong></h2>
+                    <p><strong>Especie</strong>: <?php echo $datos->especie ?></p>
+                    <p><strong>Personalidad</strong>: <?php echo $datos->personalidad ?></p>
+                    <p><strong>Muletilla</strong>: <?php echo $datos->muletilla ?></p>
+                    <p><strong>Hobby</strong>: <?php echo $datos->hobby ?></p>
+                </div>
+            </div>
+        </div>
+    </article>
+</div>
